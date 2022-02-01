1a904e639fbc957d09f6192b3f1746e57bd3874b
Ania Marecki <aniamarecki@gmail.com>
2022-02-01 16:01:57 -0500

    corrected database, added dummy data, updated dependencies

--- server/db/index.js
+++ server/db/index.js
@@ -1,5 +1,5 @@
 const Sequelize = require("sequelize");
-const db = require("../db");
+const db = require("./db");
 const FoodItem = require("./models/FoodItem");
 const Fridge = require("./models/Fridge");
 const Recipe = require("./models/Recipe");
