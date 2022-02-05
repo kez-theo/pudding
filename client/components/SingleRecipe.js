2e7676122b0a93dbdd8f3d3eae05ed5498417709
Elena Zobak <elena@Elenas-MBP.fios-router.home>
2022-02-05 11:05:00 -0500

    working singleRecipe component with thunk

--- client/components/SingleRecipe.js
+++ client/components/SingleRecipe.js
@@ -3,38 +3,40 @@ import { StatusBar } from "expo-status-bar";
 import { Button, StyleSheet, Image, Text, View } from "react-native";
 import { useSelector, useDispatch } from "react-redux";
 import { getRecipeById } from "../store/singleRecipe";
-
+//this screen should get resipeId and name as props
 export default function SingleRecipe({navigation}) {
     
-   
+    const dispatch = useDispatch();
 
-   
-    //let stepsArray = recipe[0].steps;
+    const { recipeSteps } = useSelector((state) => {
+        return {
+            recipeSteps: state.recipeReducer.steps,
+        }
+    });  
+    //need change number to recipeId (will pass from recipes screen)
+    useEffect(() => {
+       dispatch(getRecipeById(663641));
+    }, []);
+
     return (
-     
-     <View style={styles.container}>
-     <Text style={styles.text}>{dummyRecipesOptions.results[1].title} Preparation</Text> 
-     {/* {
-      stepsArray.map((item, index) => (
-        <Text key={index} style={styles.text2}>{item.number} {item.step}</Text> 
-      )
-      )}   */}
-     <Text style={styles.text}>Enjoy!</Text> 
-     <Image
-        style={styles.tinyThyme}
-        source={{
-          uri:
-            "https://us.123rf.com/450wm/eridanka/eridanka2103/eridanka210300026/165315737-a-sprig-of-rosemary-hand-drawn-sketch-style-illustration-design-element.jpg?ver=6",
-        }}
-      />
-     
 
-
-
-   </View> 
-
-
-  )
+        <View style={styles.container}>
+            <Text style={styles.text}>{dummyRecipesOptions.results[1].title} preparation</Text> 
+            {
+            recipeSteps.map((item, index) => (
+                <Text key={index} style={styles.text2}>{item.number}. {item.step}</Text> 
+            )
+            )}  
+            <Text style={styles.text}>Enjoy!</Text> 
+            <Image
+                style={styles.tinyThyme}
+                source={{
+                uri:
+                    "https://us.123rf.com/450wm/eridanka/eridanka2103/eridanka210300026/165315737-a-sprig-of-rosemary-hand-drawn-sketch-style-illustration-design-element.jpg?ver=6",
+                }}
+            />
+        </View> 
+    )
   
 }
 
@@ -61,11 +63,12 @@ const styles = StyleSheet.create({
     //fontWeight: 'bold',
     justifyContent: 'center', 
     alignItems: 'center',
-    fontFamily: 'Avenir', 
+    fontFamily: 'Avenir',
+    padding: 5, 
   },
   tinyThyme: {
-    width: 100,
-    height: 100,
+    width: 60,
+    height: 60,
   },
 });
 
@@ -88,138 +91,138 @@ const dummyRecipesOptions = {
     "number": 2,
     "totalResults": 804
 };
-const dummyRecipe = [
-    {
-        "name": "",
-        "steps": [
-            {
-                "number": 1,
-                "step": "Prepare the eggs, bread crumbs and sliced tomatoes.Dip the tomato slice in the egg wash and then in the bread crumbs.  If you have extra egg wash and bread crumbs you can repeat this process.",
-                "ingredients": [
-                    {
-                        "id": 18079,
-                        "name": "breadcrumbs",
-                        "localizedName": "breadcrumbs",
-                        "image": "breadcrumbs.jpg"
-                    },
-                    {
-                        "id": 11529,
-                        "name": "tomato",
-                        "localizedName": "tomato",
-                        "image": "tomato.png"
-                    },
-                    {
-                        "id": 1123,
-                        "name": "egg",
-                        "localizedName": "egg",
-                        "image": "egg.png"
-                    },
-                    {
-                        "id": 0,
-                        "name": "dip",
-                        "localizedName": "dip",
-                        "image": ""
-                    }
-                ],
-                "equipment": []
-            },
-            {
-                "number": 2,
-                "step": "Heat a large frying pan with the olive oil and place the prepared tomato slices in the pan.  When the tomatoes have a nice golden color gently flip and finish cooking on the other side.  This will take about 3-4 minutes on each side on medium heat.",
-                "ingredients": [
-                    {
-                        "id": 10511529,
-                        "name": "tomato slices",
-                        "localizedName": "tomato slices",
-                        "image": "sliced-tomato.jpg"
-                    },
-                    {
-                        "id": 4053,
-                        "name": "olive oil",
-                        "localizedName": "olive oil",
-                        "image": "olive-oil.jpg"
-                    },
-                    {
-                        "id": 11529,
-                        "name": "tomato",
-                        "localizedName": "tomato",
-                        "image": "tomato.png"
-                    }
-                ],
-                "equipment": [
-                    {
-                        "id": 404645,
-                        "name": "frying pan",
-                        "localizedName": "frying pan",
-                        "image": "pan.png"
-                    }
-                ],
-                "length": {
-                    "number": 4,
-                    "unit": "minutes"
-                }
-            },
-            {
-                "number": 3,
-                "step": "Place halved eggplant in the frying pan and sprinkle with dashes of salt and pepper.",
-                "ingredients": [
-                    {
-                        "id": 1102047,
-                        "name": "salt and pepper",
-                        "localizedName": "salt and pepper",
-                        "image": "salt-and-pepper.jpg"
-                    },
-                    {
-                        "id": 11209,
-                        "name": "eggplant",
-                        "localizedName": "eggplant",
-                        "image": "eggplant.png"
-                    }
-                ],
-                "equipment": [
-                    {
-                        "id": 404645,
-                        "name": "frying pan",
-                        "localizedName": "frying pan",
-                        "image": "pan.png"
-                    }
-                ]
-            },
-            {
-                "number": 4,
-                "step": "Let the eggplant saut on one side to a golden color then turn and saut on the other side.  This will take about 3  4 minutes on each side.Plate and finish with the parsley garnish, lemon juice and grated cheese.",
-                "ingredients": [
-                    {
-                        "id": 9152,
-                        "name": "lemon juice",
-                        "localizedName": "lemon juice",
-                        "image": "lemon-juice.jpg"
-                    },
-                    {
-                        "id": 11209,
-                        "name": "eggplant",
-                        "localizedName": "eggplant",
-                        "image": "eggplant.png"
-                    },
-                    {
-                        "id": 11297,
-                        "name": "parsley",
-                        "localizedName": "parsley",
-                        "image": "parsley.jpg"
-                    },
-                    {
-                        "id": 1041009,
-                        "name": "cheese",
-                        "localizedName": "cheese",
-                        "image": "cheddar-cheese.png"
-                    }
-                ],
-                "equipment": [],
-                "length": {
-                    "number": 4,
-                    "unit": "minutes"
-                }
-            }
-        ]
-    }
-]
+// const dummyRecipe = [
+//     {
+//         "name": "",
+//         "steps": [
+//             {
+//                 "number": 1,
+//                 "step": "Prepare the eggs, bread crumbs and sliced tomatoes.Dip the tomato slice in the egg wash and then in the bread crumbs.  If you have extra egg wash and bread crumbs you can repeat this process.",
+//                 "ingredients": [
+//                     {
+//                         "id": 18079,
+//                         "name": "breadcrumbs",
+//                         "localizedName": "breadcrumbs",
+//                         "image": "breadcrumbs.jpg"
+//                     },
+//                     {
+//                         "id": 11529,
+//                         "name": "tomato",
+//                         "localizedName": "tomato",
+//                         "image": "tomato.png"
+//                     },
+//                     {
+//                         "id": 1123,
+//                         "name": "egg",
+//                         "localizedName": "egg",
+//                         "image": "egg.png"
+//                     },
+//                     {
+//                         "id": 0,
+//                         "name": "dip",
+//                         "localizedName": "dip",
+//                         "image": ""
+//                     }
+//                 ],
+//                 "equipment": []
+//             },
+//             {
+//                 "number": 2,
+//                 "step": "Heat a large frying pan with the olive oil and place the prepared tomato slices in the pan.  When the tomatoes have a nice golden color gently flip and finish cooking on the other side.  This will take about 3-4 minutes on each side on medium heat.",
+//                 "ingredients": [
+//                     {
+//                         "id": 10511529,
+//                         "name": "tomato slices",
+//                         "localizedName": "tomato slices",
+//                         "image": "sliced-tomato.jpg"
+//                     },
+//                     {
+//                         "id": 4053,
+//                         "name": "olive oil",
+//                         "localizedName": "olive oil",
+//                         "image": "olive-oil.jpg"
+//                     },
+//                     {
+//                         "id": 11529,
+//                         "name": "tomato",
+//                         "localizedName": "tomato",
+//                         "image": "tomato.png"
+//                     }
+//                 ],
+//                 "equipment": [
+//                     {
+//                         "id": 404645,
+//                         "name": "frying pan",
+//                         "localizedName": "frying pan",
+//                         "image": "pan.png"
+//                     }
+//                 ],
+//                 "length": {
+//                     "number": 4,
+//                     "unit": "minutes"
+//                 }
+//             },
+//             {
+//                 "number": 3,
+//                 "step": "Place halved eggplant in the frying pan and sprinkle with dashes of salt and pepper.",
+//                 "ingredients": [
+//                     {
+//                         "id": 1102047,
+//                         "name": "salt and pepper",
+//                         "localizedName": "salt and pepper",
+//                         "image": "salt-and-pepper.jpg"
+//                     },
+//                     {
+//                         "id": 11209,
+//                         "name": "eggplant",
+//                         "localizedName": "eggplant",
+//                         "image": "eggplant.png"
+//                     }
+//                 ],
+//                 "equipment": [
+//                     {
+//                         "id": 404645,
+//                         "name": "frying pan",
+//                         "localizedName": "frying pan",
+//                         "image": "pan.png"
+//                     }
+//                 ]
+//             },
+//             {
+//                 "number": 4,
+//                 "step": "Let the eggplant saut on one side to a golden color then turn and saut on the other side.  This will take about 3  4 minutes on each side.Plate and finish with the parsley garnish, lemon juice and grated cheese.",
+//                 "ingredients": [
+//                     {
+//                         "id": 9152,
+//                         "name": "lemon juice",
+//                         "localizedName": "lemon juice",
+//                         "image": "lemon-juice.jpg"
+//                     },
+//                     {
+//                         "id": 11209,
+//                         "name": "eggplant",
+//                         "localizedName": "eggplant",
+//                         "image": "eggplant.png"
+//                     },
+//                     {
+//                         "id": 11297,
+//                         "name": "parsley",
+//                         "localizedName": "parsley",
+//                         "image": "parsley.jpg"
+//                     },
+//                     {
+//                         "id": 1041009,
+//                         "name": "cheese",
+//                         "localizedName": "cheese",
+//                         "image": "cheddar-cheese.png"
+//                     }
+//                 ],
+//                 "equipment": [],
+//                 "length": {
+//                     "number": 4,
+//                     "unit": "minutes"
+//                 }
+//             }
+//         ]
+//     }
+// ]
--- client/store/singleRecipe.js
+++ client/store/singleRecipe.js
@@ -1,14 +1,14 @@
 import axios from 'axios'
 const spnAPI = 'https://api.spoonacular.com/recipes/'
-const SPOON_API_KEY = "SPOON_API_KEY";
+import {SPOON_API_KEY} from "../.keys.js"
 
 //ACTIONS
 const GET_RECIPE = 'GET_RECIPE'
 
 //ACTION CREATORS
-export const getSingleRecipe = (recipeId) => ({
+export const getSingleRecipe = (recipe) => ({
   type: GET_RECIPE,
-  recipeId,
+  recipe,
 });
 
 
@@ -16,11 +16,11 @@ export const getSingleRecipe = (recipeId) => ({
 
 //get recipe by  recipe id
 export const getRecipeById = (recipeId) => {
-    console.log(`recipeId from getRecipeById thunk ${recipeId}`)
+   // console.log(`recipeId from getRecipeById thunk ${recipeId}`)
   return async (dispatch) => {
     try {
-        const { data: recipe } = await axios.get(`${spnAPI}${recipeId}/analyzedInstructions&apiKey=7de551d8a45043c7b1297b9a73eaf5c1`);
-        console.log(`recipe from getRecipeById thunk ${recipe}`)
+        const { data: recipe } = await axios.get(`${spnAPI}${recipeId}/analyzedInstructions?&apiKey=${SPOON_API_KEY}`);
+       // console.log(`recipe from getRecipeById thunk ${recipe}`)
         dispatch(getSingleRecipe(recipe));
         
     } catch (error) {
@@ -37,7 +37,8 @@ export default function recipeReducer(state = {}, action) {
     console.log("I'm recipeReducer")
   switch (action.type) {
     case GET_RECIPE:
-      return {recipe: action.recipe};
+    //because the url returns an array [{}]
+      return action.recipe[0];
     default:
       return state;
   }
