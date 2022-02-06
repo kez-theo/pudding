0a9bbd4264dc47c998f4568507abe706bc456609
Ania Marecki <aniamarecki@gmail.com>
2022-02-06 18:18:10 -0500

    scan crash fix

--- src/db/db.js
+++ src/db/db.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize");
 const pkg = require("../../package.json");
-require('dotenv').config();
+require("dotenv").config();
 
 const databaseName =
   pkg.name + (process.env.NODE_ENV === "test" ? "-test" : "");
@@ -15,18 +15,18 @@ if (process.env.LOGGING === "true") {
 
 //https://stackoverflow.com/questions/61254851/heroku-postgres-sequelize-no-pg-hba-conf-entry-for-host
 //It was if (process.env.DATABASE_URL) I changed to if true because the ssl wasn't working
-if (true) {
+if (process.env.DATABASE_URL) {
   config.dialectOptions = {
     ssl: {
+      require: true,
       rejectUnauthorized: false,
     },
   };
 }
-console.log("herokudb",process.env.HEROKU_DB);
+console.log("herokudb", process.env.HEROKU_DB);
 const db = new Sequelize(
-  
-  process.env.DATABASE_URL || `postgres://${process.env.HEROKU_DB}`,
-  //process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
+  //process.env.DATABASE_URL || `postgres://${process.env.HEROKU_DB}`,
+  process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
   config
 );
 module.exports = db;
