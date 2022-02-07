086ee4a801adfd9fe8f38ee68b7a39565737702a
Elena Zobak <elena@Elenas-MBP.fios-router.home>
2022-02-07 16:11:28 -0500

    dbb3

--- src/db/db.js
+++ src/db/db.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize");
 const pkg = require("../../package.json");
-require("dotenv").config();
+// require("dotenv").config();
 
 const databaseName = pkg.name + (process.env.NODE_ENV === "test" ? "-test" : "");
 
@@ -24,8 +24,8 @@ if (true) {
   };
 }
 const db = new Sequelize(
-process.env.DATABASE_URL || `postgres://${process.env.HEROKU_DB}`,
-//process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
+// process.env.DATABASE_URL || `postgres://${process.env.HEROKU_DB}`,
+process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
   config
 );
 module.exports = db;
