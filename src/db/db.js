bca92e3196462bfdf5006d16654dfcbfdef979af
Krystin Fields <krysfields12@gmail.com>
2022-02-10 18:32:04 -0500

    test2

--- src/db/db.js
+++ src/db/db.js
@@ -22,7 +22,8 @@ if (process.env.DATABASE_URL) {
 }
 
 const db = new Sequelize(
-  process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
+  process.env.DATABASE_URL || `postgres://${process.env.HEROKU_DB}`,
+  // process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
   config
 );
 module.exports = db;
