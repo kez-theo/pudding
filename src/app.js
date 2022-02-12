c6cd14d0b1e2109afcde2056dd067f2b976ae0b0
keranie theodosiou <keranie.theodosiou@gmail.com>
2022-02-12 10:22:50 -0500

    app.js revised

--- src/app.js
+++ src/app.js
@@ -12,6 +12,10 @@ if (process.env.NODE_ENV !== "production") {
   require("../.keys");
 }
 
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
 // logging middleware
 app.use(morgan("dev"));
 
